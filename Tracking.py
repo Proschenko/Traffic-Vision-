b2c4ec3a893410cf9737df2d8905d0740cbf922a
Iversy <iversy49@ya.ru>
2024-03-05 21:59:11 +0500

    Dynamic resolution detection

--- Tracking.py
+++ Tracking.py
@@ -10,6 +10,7 @@ class People:
         self.model_class: str = class_person  # если циферками класс задается то можно в случае чего преобразовывать в строку
         self.confidence: float = conf
 
+        #Корды не нормализованы
         self.center_x: int = None
         self.center_y: int = None
 
@@ -17,19 +18,20 @@ class People:
 
     def _set_coordinates(self, coordinates):
         """
-        Ставим новые нормализованные координаты
+        Ставим новые координаты
         """
         self.center_x = coordinates[0]
         self.center_y = coordinates[1]
 
     # Смотрим насколько близко находится к двери
     def check_how_close_to_door(self):
-        # door_centers = [Doors.kid_center_door.value, Doors.women_center_door.value, Doors.men_center_door.value]
-        door_centers = [Doors.women_center_door.value]
+        door_centers = [Doors.kid_center_door.value, Doors.women_center_door.value, Doors.men_center_door.value]
+        #door_centers = [Doors.women_center_door.value]
 
         for door_center in door_centers:
             print("People", self.center_x, self.center_y, "Doors", door_center[0], door_center[1])
             distance_to_door = numpy.sqrt((self.center_x - door_center[0]) ** 2 + (self.center_y - door_center[1]) ** 2)
+            distance_to_door = Tracking().get_normalized_coordinates((distance_to_door,0))[0]
             print(distance_to_door)
             if distance_to_door < 50:
                 self.print_person()
@@ -46,55 +48,29 @@ class People:
     def print_person(self):
         print("Class:", self.model_class)
         print("Confidence:", self.confidence)
-        print("X:", self.center_x)
-        print("Y:", self.center_y)
+        norm_x, norm_y =  Tracking().get_normalized_coordinates((self.center_x, self.center_y))
+        print("X:", self.center_x, "Нормализованый:", norm_x)
+        print("Y:", self.center_y, "Нормализованый:", norm_y)
 
 
 class Doors(enum.Enum):
-    women_center_door = (185, 175)
-    men_center_door = (248, 151)
-    kid_center_door = (564, 93)
-
-
-# class Door:
-#     def __init__(self) -> None:
-#         self.women_center_door = (0.14453125, 0.244140625)
-#         self.men_center_door = (0.19375, 0.2109375)
-#         self.kid_center_door = (0.440625, 0.13046875000000002)
-
-
-#     def get_women_door_coordinates(self):
-#         """
-#         Возвращает абсолютные координаты центра дверей для женщин.
-#         """
-#         return Tracking.get_normalized_coordinates(self.women_center_door)
-
-#     def get_men_door_coordinates(self):
-#         """
-#         Возвращает абсолютные координаты центра дверей для мужчин.
-#         """
-#         return Tracking.get_normalized_coordinates(self.men_center_door)
-
-#     def get_kid_door_coordinates(self):
-#         """
-#         Возвращает абсолютные координаты центра дверей для детей.
-#         """
-#         return Tracking.get_normalized_coordinates(self.kid_center_door)
+    women_center_door = (0.14453125, 0.244140625)
+    men_center_door = (0.19375, 0.2109375)
+    kid_center_door = (0.440625, 0.13046875000000002)
 
 
 class Tracking:
     def __init__(self) -> None:
-        pass
+        self.image_width = 1920
+        self.image_height = 1080
 
-    @staticmethod
-    def get_normalized_coordinates(center):
+    #Для динамического определения разрешения убрал декоратор @staticmethod, теперь обращение по объекту, т.е. Tracking().get_normalized_coordinates()
+    def get_normalized_coordinates(self, center):
         """
         Преобразует нормализованные координаты центра в абсолютные координаты для разрешения изображения.
         """
-        image_width = 1920
-        image_height = 1080
-        absolute_x = int(center[0] * image_width)
-        absolute_y = int(center[1] * image_height)
+        absolute_x = int(center[0] * self.image_width)
+        absolute_y = int(center[1] * self.image_height)
         return absolute_x, absolute_y
 
     def process_video_with_tracking(self, model, input_video_path, show_video=True, save_video=False,
@@ -109,7 +85,11 @@ class Tracking:
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+        
+        #Ставим актуальное разрешение
+        self.image_width = frame_width
+        self.image_height = frame_height
+        
         # Define the output video writer
         if save_video:
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
