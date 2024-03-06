c7107a3ffafc735292751c320789e1799319101f
Oleg Pepeg <bakarbyikov@gmail.com>
2024-03-06 21:34:42 +0500

    Removed class

--- OperationsWithCordinates.py
+++ OperationsWithCordinates.py
@@ -1,18 +1,16 @@
 import numpy as np
 
 
-class OperationsWithCoordinates():
-    @staticmethod
-    def boxes_center(corners: np.ndarray[float, float]) -> np.ndarray[float, float]:
-        """
-        Находит центры прямоугольников, которые заданны двумя углами.
+def boxes_center(corners: np.ndarray[float, float]) -> np.ndarray[float, float]:
+    """
+    Находит центры прямоугольников, которые заданны двумя углами.
 
-        :param corners: Массив координат размера (n, 4) формата xyxy
-        :type corners: np.ndarray[float, float]
-        :return: Массив координат размера (n, 2) формата xy
-        :rtype: np.ndarray[float, float]
-        """
-        return np.mean(corners.reshape((-1, 2, 2)), 1)
+    :param corners: Массив координат размера (n, 4) формата xyxy
+    :type corners: np.ndarray[float, float]
+    :return: Массив координат размера (n, 2) формата xy
+    :rtype: np.ndarray[float, float]
+    """
+    return np.mean(corners.reshape((-1, 2, 2)), 1)
 
     # @staticmethod
     # def xxx(coordinate):
