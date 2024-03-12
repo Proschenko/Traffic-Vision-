25886ffebcc2e5c85fb98cb74f92544aecca5f45
Iversy <iversy49@ya.ru>
2024-03-12 16:41:38 +0500

    Refactoring tracking

--- Tracking.py
+++ Tracking.py
@@ -70,28 +70,36 @@ class Tracking:
         :param results:
         :return:
         """
-        # TODO: этот код нужно поделить на методы, каждый методы (зашел вышел прошел)
-        for person in parse_results(results):
-            now = person.check_how_close_to_door()
-            if person.id_person not in self.id_location:
-                newborn = now is Location.Close
-                self.id_location[person.id_person] = State(now, newborn)
-                if newborn:
-                    self.in_out[0] += 1
-                    nearest_door = person.nearest_door()
-                    print("Я родился!", nearest_door.name)
-                continue
-            state = self.id_location[person.id_person]
-            before = state.location
-            if now is Location.Close and before is Location.Around:
-                self.in_out[1] += 1
-                nearest_door = person.nearest_door()
-                print("Я вышел!", nearest_door.name)
-            if not state.newborn and now is Location.Around and before is Location.Close:
-                self.in_out[1] -= 1
-                nearest_door = person.nearest_door()
-                print("Погодите-ка, я просто мимо проходил", nearest_door.name)
-            self.id_location[person.id_person].update(now)
+        people = parse_results(results)
+        for person in people:
+            current_location = person.check_how_close_to_door()
+            last_state, last_newborn = self.update_location_state(person, current_location)
+            if last_state:
+                self.handle_entry(person, last_state, last_newborn, current_location)
+        
+    def handle_entry(self, person: People, last_location: State, last_newborn: bool, current_location: Location):
+        nearest_door = person.nearest_door()
+        message = ""
+        # print(last_state.location, current_location)
+        if current_location is Location.Close and last_location is Location.Around:
+            self.in_out[1] += 1
+            message = "Я вышел!"
+        if not last_newborn and current_location is Location.Around and last_location is Location.Close:
+            self.in_out[1] -= 1
+            message = "Погодите-ка, я просто мимо проходил"
+        if message:
+            print(message, nearest_door.name)
+
+    def update_location_state(self, person: People, location: Location) -> Location:
+        if person.id_person not in self.id_location:
+            self.id_location[person.id_person] = State(location)
+            self.in_out[0] += 1
+            nearest_door = person.nearest_door()
+            print("Я родился!", nearest_door.name)
+            return None, None
+        last_location, last_newborn = self.id_location[person.id_person].location, self.id_location[person.id_person].newborn
+        self.id_location[person.id_person].update(location)
+        return last_location, last_newborn
 
     def _tracking(self):
         # TODO: Так будет работать логика будущего, сначала парсинг result, потом парсинг массива каждым методом
