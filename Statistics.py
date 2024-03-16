b04b44abb9a6b9d3682727cbdd5db8da11cf60b7
Iversy <iversy49@ya.ru>
2024-03-16 21:51:02 +0500

    not done yet

--- Statistics.py
+++ Statistics.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 def water_spilled(people: list) -> int:
     """
@@ -11,5 +12,28 @@ def water_spilled(people: list) -> int:
     """
     return len(people) * 50 
 
-def hist_pool_load():
-    pass
+def hist_pool_load(time: list, in_people: list, out_people: list) -> None:
+    times = [datetime.strptime(time_str, "%H-%M-%S") for time_str in time]
+    hourly_in = {}
+    hourly_out = {}
+    for time, in_val, out_val in zip(times, in_people, out_people):
+        hour = time.hour
+        if hour not in hourly_in:
+            hourly_in[hour] = 0
+        if hour not in hourly_out:
+            hourly_out[hour] = 0
+        hourly_in[hour] += in_val
+        hourly_out[hour] += out_val
+    
+    hours = list(hourly_in.keys())
+    values = [hourly_in[hour] for hour in hours]
+
+    plt.bar(hours, values, color='blue', alpha=0.7, label='Вошло людей')
+    plt.xlabel('Часы')
+    plt.ylabel('Количество людей')
+    plt.title('Гистограмма входа и выхода людей по часам')
+    plt.show()
+    
+if __name__ == "__main__":
+    hist_pool_load(["00-00-00", "00-10-00", "02-00-00"], [1, 2, 3], [2, 3, 4])
+    
