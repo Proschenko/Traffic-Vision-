a36886c1e0d47cfc5f008c7a5c20c1a639b95094
Oleg Pepeg <bakarbyikov@gmail.com>
2024-04-21 12:25:35 +0500

    Updated creater

--- Train/dataset_creater.py
+++ Train/dataset_creater.py
@@ -7,6 +7,7 @@ from itertools import count
 from multiprocessing import freeze_support
 from time import sleep
 from typing import Generator
+from time import time
 
 import cv2
 from cv2.typing import MatLike
@@ -14,10 +15,10 @@ from tqdm import tqdm
 from ultralytics import YOLO
 
 from Tracker.Debug_drawer import draw_debug
-from Tracker.misc import crop_image, frame_crop, fill_black
-from Tracker.People import People
+from Tracker.misc import crop_image
+from Tracker.People import People, Gender
 from Tracker.StreamCatcher import ParallelStream
-from Tracker.Tracking import Tracking
+from Tracker.Tracking import parse_results
 
 
 def save() -> Generator[None, tuple[MatLike, list[People]], None]:
@@ -26,41 +27,38 @@ def save() -> Generator[None, tuple[MatLike, list[People]], None]:
     for i in count():
         image, persons = yield
         name = f"frame_{i:08}"
-        for oleg in sorted(p.model_class for p in persons):
+        for oleg in sorted(p.gender.name.lower() for p in persons):
             name += f"_{oleg}"
+        # print(f"New frame {name}")
         cv2.imwrite(directory+name+".png", image)
 
 
 if __name__ == "__main__":
     freeze_support()
-    model_args = {"iou": 0, "conf": 0.6, "classes":(1, 2, 3),
+    classes = [g.value for g in (Gender.Kid, Gender.Man, Gender.Woman)]
+    model_args = {"iou": 0.4, "conf": 0.5, "classes":classes,
                 "imgsz": 640, "verbose": False}
-    model = YOLO('runs/detect/train2/weights/best.pt')
-    Oleg = "Pepeg"
-    # tracker = Tracking(model)
-
-    # path = ("D:/я у мамы программист/3 курс 2 семестр IT-проекты"
-    #         "/Traffic-Vision-/Test input video/test.mp4")
+    model = YOLO('runs/detect/train8/weights/best.pt')
     url = "rtsp://rtsp:EL3gS7XV@80.91.19.85:58002/Streaming/Channels/101"
     stream = ParallelStream(url)
-    # stream.jump_to(39*60)
 
-    # frame_step = 40
-    # total = (stream.n_frames - stream.frame_number) // frame_step
+    step = 15
     prev = None
+    last_time = 0
     saver = save()
     saver.send(None)
-    for frame in tqdm(stream.iter_actual(), disable=False):
-        frame = crop_image(frame, **frame_crop)
-        frame = fill_black(frame)
-        detected = model.track(frame, **model_args)[0]
-        persons = Tracking.parse_results(Oleg, detected)
+    with tqdm() as pbar:
+        for frame in stream.iter_actual():
+            frame = crop_image(frame)
+            detected = model.track(frame, **model_args)[0]
+            persons = parse_results(detected)
 
-        cv2.imshow("frame", draw_debug(detected, persons, draw_lines=False))
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+            cv2.imshow("frame", draw_debug(detected, persons, draw_lines=False))
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
-        if len(persons) > 1:
-            # saver.send((frame, persons))
-            sleep(1)
-        prev = persons
+            if prev and len(persons) < len(prev) > 1 and (time()-last_time >= step):
+                pbar.update(1)
+                saver.send((frame, prev))
+                last_time = time()
+            prev = persons
