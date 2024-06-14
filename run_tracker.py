4b0583540392b2be431cce3fb740d0b0258e9308
MaGmYmS <108451712+MaGmYmS@users.noreply.github.com>
2024-06-14 15:51:40 +0500

    private rstp

--- run_tracker.py
+++ run_tracker.py
@@ -2,8 +2,9 @@ from Tracker.main import Master
 
 
 def main():
-    url = 'rtsp://rtsp:EL3gS7XV@80.91.19.85:58002/Streaming/Channels/101'
+    url = ""
     Master((url, url)).run()
+
 
 if __name__ == "__main__":
     main()
